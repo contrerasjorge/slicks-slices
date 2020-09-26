e1051489317e48848094008b0267c346ff1deb9d
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-26 15:50:04 -0400

    created custom hook for client side data fetching

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -74,13 +74,6 @@ const GlobalStyles = createGlobalStyle`
   img {
     max-width: 100%;
   }
-
-  .tilt {
-    transform: rotate(-2deg);
-    position: relative;
-    display: inline-block;
-  }
-
 `;
 
 export default GlobalStyles;
